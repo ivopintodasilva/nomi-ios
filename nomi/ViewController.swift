488a3d4992e6a6bec6373be001d53fa73c8bae03
ivopintodasilva <ivopintodasilva@gmail.com>
2015-12-16 23:26:58 +0000

    auto layout with scroll implemented in login and register

--- nomi/ViewController.swift
+++ nomi/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var tf_email: UITextField!
-    @IBOutlet weak var tf_password: UITextField!
+    
+    var activeField: UITextField?
+    
+    @IBOutlet weak var scroll_view: UIScrollView!
+    @IBOutlet weak var content_view: UIView!
+
+    @IBOutlet weak var email_text: UITextField!
+    @IBOutlet weak var password_text: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        tf_email.delegate = self
-        tf_password.delegate = self
         self.setNeedsStatusBarAppearanceUpdate()
+        email_text.delegate = self
+        password_text.delegate = self
+        registerForKeyboardNotifications()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,31 +34,91 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-
-    @IBAction func b_login() {
-        print(tf_email.text)
-        print(tf_password.text)
+    @IBAction func button_login_click() {
+        print(email_text.text)
+        print(password_text.text)
         performSegueWithIdentifier("login", sender: self)
     }
-    
-    
-    @IBAction func b_sender() {
+    @IBAction func button_register_click() {
         print("register")
     }
-    
-    /*  hides keyboard on return-pressed  */
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    /*  hides keyboard on view touch  */
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        self.view.endEditing(true)
-    }
+
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
     }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    
+    func registerForKeyboardNotifications()
+    {
+        //Adding notifies on keyboard appearing
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    
+    func deregisterFromKeyboardNotifications()
+    {
+        //Removing notifies on keyboard appearing
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notification: NSNotification)
+    {
+        //Need to calculate keyboard exact size due to Apple suggestions
+        scroll_view.scrollEnabled = true
+        var info : NSDictionary = notification.userInfo!
+        var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
+        var contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize!.height, 0.0)
+        
+        scroll_view.contentInset = contentInsets
+        scroll_view.scrollIndicatorInsets = contentInsets
+        
+        var aRect : CGRect = self.view.frame
+        aRect.size.height -= keyboardSize!.height
+        if let activeFieldPresent = activeField
+        {
+            if (!CGRectContainsPoint(aRect, activeField!.frame.origin))
+            {
+                scroll_view.scrollRectToVisible(activeField!.frame, animated: true)
+            }
+        }
+        
+        
+    }
+    
+    
+    func keyboardWillBeHidden(notification: NSNotification)
+    {
+        //Once keyboard disappears, restore original positions
+        var info : NSDictionary = notification.userInfo!
+        var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
+        var contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, -keyboardSize!.height, 0.0)
+        scroll_view.contentInset = contentInsets
+        scroll_view.scrollIndicatorInsets = contentInsets
+        self.view.endEditing(true)
+        scroll_view.scrollEnabled = false
+        
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField!)
+    {
+        activeField = textField
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField!)
+    {
+        activeField = nil
+    }
+    
+    
+    
 }
 
