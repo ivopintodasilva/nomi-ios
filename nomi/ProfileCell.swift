4510d6dc06636fa07a472813552bf9a5a28c3f05
ivopintodasilva <ivopintodasilva@gmail.com>
2016-01-04 00:18:03 +0000

    profile list created and floating button added

---
+++ nomi/ProfileCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCell.swift
+//  nomi
+//
+//  Created by Ivo Silva on 03/01/16.
+//  Copyright © 2016 Ivo Silva. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+}
--- nomi/ProfilesViewController.swift
+++ nomi/ProfilesViewController.swift
@@ -9,14 +9,27 @@
 import UIKit
 
 
-class ProfilesViewController: UIViewController {
+class ProfilesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var new_profile_btn: UIButton!
+    @IBOutlet weak var profile_table: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setNeedsStatusBarAppearanceUpdate()
-
+        
+        new_profile_btn.backgroundColor = UIColor(red: 0.71, green: 0.23, blue: 0.21, alpha: 1)
+        new_profile_btn.layer.cornerRadius = new_profile_btn.frame.size.width / 2
+        new_profile_btn.clipsToBounds = true
+        
         // Do any additional setup after loading the view.
+        
+        profile_table.delegate = self
+        profile_table.dataSource = self
+        
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,14 +41,14 @@ class ProfilesViewController: UIViewController {
         return .Default
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return UserProfilesModel.sharedInstance.user_profiles.count
     }
-    */
 
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ProfileCell", forIndexPath: indexPath) as! ProfileCell
+        cell.titleLabel.text = UserProfilesModel.sharedInstance.user_profiles[indexPath.row].name
+        return cell
+    }
 }
